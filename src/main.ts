6fd49bd78f1b61d2570d8bfd938dc8a6ff644dcc
sajidsarkar-dev <sajid.sarkar@fleekitsolutions.com>
2025-11-13 19:49:35 +0530

    app config updated

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TimingInterceptor } from './common/interceptor/timing.interceptor';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new TimingInterceptor());
+  app.useGlobalPipes(new ValidationPipe({ 
+    whitelist: true, 
+    forbidNonWhitelisted: true,
+    transform: true
+  }));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
